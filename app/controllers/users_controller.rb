1d8f981e0d5634a264d53118099b3ca6a902c599
EC2 Default User <ec2-user@ip-172-31-0-244.us-east-2.compute.internal>
2022-03-12 04:31:37 +0000

    create_showpages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def new
   end
+  
+  def show
+    #　ユーザーの情報を取得
+    #　viewファイルにその情報を送信
+    @user = User.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/sign_up', to: 'users#new'
   root 'static_pages#hello'
+  resources :users
 end
