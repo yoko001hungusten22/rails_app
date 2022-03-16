9950ca3303a3c45bcca43f7d70f69473a5605d6e
EC2 Default User <ec2-user@ip-172-31-0-244.us-east-2.compute.internal>
2022-03-16 14:02:48 +0000

    エラ〜メッセージを表示

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/hello', to: 'static_pages#hello'
   get '/about', to: 'static_pages#about'
   get '/sign_up', to: 'users#new'
+  post '/sign_up', to: 'users#create'
   root 'static_pages#hello'
   resources :users
 end
