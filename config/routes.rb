a0989ba8ed624c175937dde102dff89251228b74
EC2 Default User <ec2-user@ip-172-31-0-244.us-east-2.compute.internal>
2022-03-18 15:24:05 +0000

    login,logout機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/sign_up', to: 'users#new'
   post '/sign_up', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   root 'static_pages#hello'
   resources :users
 end
