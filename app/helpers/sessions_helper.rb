311143b4e34ca56f5416644db7f1314067e52cee
EC2 Default User <ec2-user@ip-172-31-0-244.us-east-2.compute.internal>
2022-03-20 04:15:56 +0000

    add_user_crud

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -18,4 +18,13 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+  
+  def store_url
+    session[:forwarding_url] = request.original_url
+  end
+  
+  def redirect_to_stored_url(user)
+    redirect_to session[:forwarding_url] || user
+    session.delete(:forwarding_url)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do |i|
+  User.create!(name: "hogehoge#{i}",
+              email: "hogehoge#{i}@example.com",
+              password: "foobar",
+              password_confirmation: "foobar")
+end
