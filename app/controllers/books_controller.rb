9c43d367b88b307aa6b58a5c0ff6f2920d01d6a2
EC2 Default User <ec2-user@ip-172-31-0-244.us-east-2.compute.internal>
2022-03-27 04:59:24 +0000

    部分テンプレート追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,5 @@
 class BooksController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index,:new, :create, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   include SessionsHelper
   
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :description, presence: true
 end
